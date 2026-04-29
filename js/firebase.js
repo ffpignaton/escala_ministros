e6ace4b46d57be499667eae6a4a06bd1186e1789
Fabiano Fabri Pignaton <66757957+ffpignaton@users.noreply.github.com>
2026-04-29 14:22:20 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "SUA_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyBr6qDi7cyirnC_akb134OAzjFxTOdGY4U",
+  authDomain: "douro-acessorios.firebaseapp.com",
+  projectId: "douro-acessorios",
+  storageBucket: "douro-acessorios.firebasestorage.app",
+  messagingSenderId: "281702603904",
+  appId: "1:281702603904:web:0aeda1db82e9e211635a65",
+  measurementId: "G-L1ED3TQ3M2"
 };
 
 if (!firebase.apps.length) {
