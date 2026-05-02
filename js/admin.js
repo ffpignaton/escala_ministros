4fccb09c510f7a2feb2313fd0344903e0547de2f
Fabiano Fabri Pignaton <66757957+ffpignaton@users.noreply.github.com>
2026-05-02 17:57:40 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -282,7 +282,7 @@ window.gerarPDF = function() {
             doc.setFontSize(18);
             doc.text("Paróquia Santíssima Trindade - Matriz São Jorge", 30, 20); // Alinha título à direita do logo
             doc.setFontSize(10)
-            doc.text("Escala Ministros Extraordinários da Distribuição da Sagrada Comunhão", 30, 28);
+            doc.text("Escala Ministros Extraordinários da Distribuição da Sagrada Comunhão", 30, 24);
             y = 35;
 
             let agrupado = {};
@@ -298,7 +298,7 @@ window.gerarPDF = function() {
 
             // Exibe cada data e suas escalas
             for (let data in agrupado) {
-                doc.setFontSize(10);
+                doc.setFontSize(5);
                 doc.setFont(undefined, "bold");
                 doc.text(formatarDataCompleta(data), 10, y);
                 y += 5;
