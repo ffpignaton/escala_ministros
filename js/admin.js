858abfcb19d18e9f7fbdbebc2bb2855ec52681cf
Fabiano Fabri Pignaton <66757957+ffpignaton@users.noreply.github.com>
2026-05-03 08:24:56 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -11,7 +11,7 @@ let calendarAdmin = null;
 /* =========================================
 ABRIR TELAS
 ========================================= */
-window.abrirTela = function(id){
+window.abrirTela = function(id) {
     // Esconde todas as telas
     document.querySelectorAll(".tela").forEach(sec => {
         sec.classList.add("hidden");
@@ -38,11 +38,11 @@ window.abrirTela = function(id){
 /* =========================================
 MINISTROS
 ========================================= */
-window.salvarMinistro = function(){
+window.salvarMinistro = function() {
     let nome = document.getElementById("nome").value.trim();
     let fone = document.getElementById("fone").value.trim();
 
-    if(!nome){
+    if (!nome) {
         alert("Digite o nome.");
         return;
     }
